129ecdc1abf8a575adb279fee142807c16961550
Diego Castillo <diegocasmo@gmail.com>
2016-01-14 20:08:49 -0500

    Cars are paginated as user scrolls down the page

--- frontend/app/scripts/views/base_view.js
+++ frontend/app/scripts/views/base_view.js
@@ -18,6 +18,22 @@ define([
         childView.remove();
       });
       this.remove();
+    },
+
+    // Renders a particular view on the specified element
+    renderSubView: function(options) {
+      var view = new options.View(options.context);
+      this.subViews.push(view);
+      options.$el.html(view.render().el);
+    },
+
+    scrollToTop: function() {
+      $('html,body').animate({ scrollTop: 0 }, 'slow');
+    },
+
+    hideComponent: function($el) {
+      $el.addClass('display-none')
+         .removeClass('display-block');
     }
 
   });
